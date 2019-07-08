ed0836c2ac523abd42afbb2f7be1dd7c064387e8
Ambuj Kumar <itsambuja@gmail.com>
2019-07-08 17:32:35 +0530

    basic page created and deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC_gbKcOmia_TFvfWgbfkNWEsLOMYHpVaI",
+    authDomain: "oshop-60f80.firebaseapp.com",
+    databaseURL: "https://oshop-60f80.firebaseio.com",
+    projectId: "oshop-60f80",
+    storageBucket: "",
+    messagingSenderId: "1059580705433",
+    appId: "1:1059580705433:web:91e0b44a3aff44a3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC_gbKcOmia_TFvfWgbfkNWEsLOMYHpVaI",
+    authDomain: "oshop-60f80.firebaseapp.com",
+    databaseURL: "https://oshop-60f80.firebaseio.com",
+    projectId: "oshop-60f80",
+    storageBucket: "",
+    messagingSenderId: "1059580705433",
+    appId: "1:1059580705433:web:91e0b44a3aff44a3"
+  }
 };
 
 /*
